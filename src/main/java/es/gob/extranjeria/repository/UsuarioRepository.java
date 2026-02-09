034ba3aa5786266e0536ebad95d40a94f0bf2416
Jaimotas <roblesmorenojaime@gmail.com>
2026-02-09 12:52:44 +0100

    Arreglo con revert

--- src/main/java/es/gob/extranjeria/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package es.gob.extranjeria.repository;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-import es.gob.extranjeria.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario,Long > {
-    Optional<Usuario> findByUsername(String username);
-}
