99d4c13ae570f2be2995a1874712e1084e53ca3e
Jaimotas <roblesmorenojaime@gmail.com>
2026-02-09 12:35:14 +0100

    Mejoras diseño CargaInterfaz

---
+++ src/test/java/es/gob/extranjeria/JanusApplicationTests.java
@@ -0,0 +1,13 @@
+package es.gob.extranjeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
